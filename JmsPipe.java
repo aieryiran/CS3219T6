ca60f1cce0fa654305d1e31c02e69b22e29aab9c
Su Gi Chandran <sollercoaster@hotmail.com>
2015-10-06 20:17:15 +0800

    Working version of JmsPipe

--- JmsPipe.java
+++ JmsPipe.java
@@ -1,11 +1,4 @@
-package order;
-
 import java.util.Properties;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Properties;
-
 import javax.jms.*;
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -15,60 +8,148 @@ import javax.naming.NamingException;
  * Matric 1: A0105610Y
  * Name   1: Wen Yiran
  * 
- * Matric 2:
- * Name   2:
+ * Matric 2: A0101642X
+ * Name   2: Su Gi Chandran
  *
  * This file implements a pipe that transfer messages using JMS.
  */
 
-public class JmsPipe implements IPipe {
+public class JmsPipe implements IPipe, MessageListener {
     
-    // your code here
-    private QueueConnectionFactory qconFactory;
-    private QueueConnection qcon;
-    private QueueSession qsession;
+    // Properties
+    private QueueConnectionFactory readConFactory, writeConFactory;
+    private QueueConnection readCon, writeCon;
+    private QueueSession readSession, writeSession;
     private QueueSender qsender;
     private QueueReceiver qreceiver;
-    private Queue queue;
+    private Queue readQueue, writeQueue;
     private TextMessage msg;
+    private String msgText, factory, queuename;
+    private boolean initRead = false, initWrite = false;
 	
-	JmsPipe(String arg1, String arg2) throws NamingException, JMSException {
-		InitialContext ic = getInitialContext();
-		qconFactory = (QueueConnectionFactory) ic.lookup(arg1);
-        qcon = qconFactory.createQueueConnection();
-        qsession = qcon.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
-        queue = (Queue) ic.lookup(arg2);
-        qreceiver = qsession.createReceiver(queue);
-        //qreceiver.setMessageListener(this);
-        qcon.start();
+    // Constructor
+	JmsPipe(String factory, String queuename) {
+		this.factory = factory;
+		this.queuename = queuename;
 	}
 	
+	// Initialise a receiver
+	public void initRead() throws NamingException, JMSException{
+		if(!initRead){
+			initRead = true;
+		}
+		else {
+			return;
+		}
+		
+		InitialContext ic = getInitialContext();
+		readConFactory = (QueueConnectionFactory) ic.lookup(factory);
+        readCon = readConFactory.createQueueConnection();
+        readSession = readCon.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+        readQueue = (Queue) ic.lookup(queuename);
+        qreceiver = readSession.createReceiver(readQueue);
+        qreceiver.setMessageListener(this);
+		readCon.start();
+	}
+	
+	// Initalise a sender
+	public void initWrite() throws NamingException, JMSException{
+		if(!initWrite){
+			initWrite = true;
+		}
+		else {
+			return;
+		}
+		
+		InitialContext ic = getInitialContext();
+        writeConFactory = (QueueConnectionFactory) ic.lookup(factory);
+        writeCon = writeConFactory.createQueueConnection();
+        writeSession = writeCon.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+        writeQueue = (Queue) ic.lookup(queuename);
+        qsender = writeSession.createSender(writeQueue);
+        msg = writeSession.createTextMessage();
+        writeCon.start();
+	}
+	
+	@Override
+	// Write a message to output
 	public void write(Order s) {
         try {
-			send(s.toString());
+        	initWrite();
+        	if (s.toString() != null && s.toString().trim().length() != 0) {
+        		send(s.toString());
+        	}
+        	else {
+        		System.out.println("Invalid message.");
+        		return;
+        	}
 		} catch (JMSException e) {
-			// TODO Auto-generated catch block
+			System.err.println("A JMS exception has occurred:");
+			e.printStackTrace();
+		} catch (NamingException e) {
+			System.err.println("A NamingException has occurred:");
 			e.printStackTrace();
 		}
 	} 
 	
+	// Utility function used by write()
 	private void send(String message) throws JMSException {
         msg.setText(message);
         qsender.send(msg);
     }
 	
+	// Event listener for receiving messages
+	public void onMessage(Message msg) {
+        try {
+            if (msg instanceof TextMessage) {
+                msgText = ((TextMessage) msg).getText();
+            } else {
+                msgText = msg.toString();
+            }
+        } catch (JMSException jmse) {
+            System.err.println("A JMS exception occurred: " + jmse.getMessage());
+        }
+	}
+	
+	@Override
+	// Read a message captured by the event listener
     public Order read() {
+    	try {
+    		initRead();
+    		
+        	while(true){
+            	if(msgText != null) {
+            		String tempMsg = msgText;
+            		msgText = null;
+            		return Order.fromString(tempMsg);
+            	}
+        	}
+    	} catch (JMSException e) {
+			System.err.println("A JMS exception has occurred:");
+			e.printStackTrace();
+		} catch (NamingException e) {
+			System.err.println("A NamingException has occurred:");
+			e.printStackTrace();
+		}
+    	
     	return null;
     }
     
+	@Override
     public void close() {
         try {
-        	qsender.close();
-        	qreceiver.close();
-			qsession.close();
-			qcon.close();
+        	if(initRead){
+        		qsender.close();
+        		readSession.close();
+        		readCon.close();
+        	}
+        	if(initWrite){
+        		qreceiver.close();
+        		writeSession.close();
+        		writeCon.close();
+        	}	
 		} catch (JMSException e) {
-			// TODO Auto-generated catch block
+			System.err.println("A JMS exception has occurred:");
 			e.printStackTrace();
 	 	}
     }
